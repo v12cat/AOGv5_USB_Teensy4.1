267ad7b26ca6dd9c2a147539c41d80c9431fe42b
v12cat <52757113+v12cat@users.noreply.github.com>
2021-04-10 00:41:06 +0100

    Update AutosteerPID.ino

--- Autosteer_USB_20_CAN/AutosteerPID.ino
+++ Autosteer_USB_20_CAN/AutosteerPID.ino
@@ -26,28 +26,6 @@ void calcSteeringPID(void)
 
   if (steerConfig.MotorDriveDirection) pwmDrive *= -1;
 
-//+++++++++++++++++++++++++++ CAN +++++++++++++++++++++++++++++
-  int16_t shift = (sq(pwmDrive)/32);
-  if (pwmDrive< 0) shift *= -1;
-  canSteerPosition = canSteerPosition + shift;
-    if (canSteerPosition > 32767) canSteerPosition = 32767;
-    if (canSteerPosition < -32768) canSteerPosition = -32768;
-
-//-------------------------- CAN ---------------------------------
-
-  if (steerConfig.isDanfoss)
-  {
-    // Danfoss: PWM 25% On = Left Position max  (below Valve=Center)
-    // Danfoss: PWM 50% On = Center Position
-    // Danfoss: PWM 75% On = Right Position max (above Valve=Center)
-    pwmDrive = (constrain(pwmDrive, -250, 250));
-
-    // Calculations below make sure pwmDrive values are between 65 and 190
-    // This means they are always positive, so in motorDrive, no need to check for
-    // steerConfig.isDanfoss anymore
-    pwmDrive = pwmDrive >> 2; // Devide by 4
-    pwmDrive += 128;          // add Center Pos.
-  }
  }
 
 //#########################################################################################
